b774ef763f1c1eaaca7a3a2caae7eecb8b894e23
ronzulu <75528127+ronzulu@users.noreply.github.com>
2025-02-12 22:55:23 +1100

    Update segbot-arduino-prototype.ino

--- segbot-arduino-prototype.ino
+++ segbot-arduino-prototype.ino
@@ -26,8 +26,19 @@
 
 int sensorPin = A0;   // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
+int analogOutput = 0;
 int count = 0;
-#define  REF_VOLTAGE    5000
+#define  MCP4725_REF_VOLTAGE    5000
+#define SEGWAY_LEFT   811
+#define SEGWAY_CENTER   511
+#define SEGWAY_RIGHT   211
+#define SEGWAY_RANGE ((long)SEGWAY_LEFT - SEGWAY_RIGHT)
+#define SEGWAY_MIN  SEGWAY_RIGHT
+
+#define NINEBOT_LEFT  2900
+#define NINEBOT_CENTER  2300
+#define NINEBOT_RIGHT  1700
+#define NINEBOT_RANGE ((long)NINEBOT_LEFT - NINEBOT_RIGHT)
 
 DFRobot_MCP4725 DAC;
 
@@ -36,26 +47,53 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
-  DAC.init(MCP4725A0_IIC_Address0, REF_VOLTAGE);
+  DAC.init(MCP4725A0_IIC_Address0, MCP4725_REF_VOLTAGE);
 
   delay(2000);
 
-  Serial.println("segbot-arduino-prototype: v1");
+  Serial.println("segbot-arduino-prototype: v2");
+}
+
+// 
+int convertSegwaySensorVoltageToNinebot(int segwaySensor) {
+  if (segwaySensor < SEGWAY_RIGHT)
+    segwaySensor = SEGWAY_RIGHT;
+  else if (segwaySensor > SEGWAY_LEFT)
+    segwaySensor = SEGWAY_LEFT;
+
+  // Convert to Left=1000, Right=0
+  long scaled = (((long)segwaySensor - SEGWAY_MIN) * 1000) / SEGWAY_RANGE;
+  Serial.print("scaled: ");
+    Serial.print(String(scaled, 10));
+
+  // long t = ((scaled * NINEBOT_RANGE) / 1000) + NINEBOT_LEFT;
+  long t = (scaled * NINEBOT_RANGE);
+  Serial.print(", t: ");
+    Serial.print(String(t, 10));
+  long t2 = t / 1000;
+  Serial.print(", t2: ");
+    Serial.println(String(t2, 10));
+return t2 + NINEBOT_RIGHT;
 }
 
 // the loop function runs over and over again forever
 void loop() {
   sensorValue = analogRead(sensorPin);
+  analogOutput = convertSegwaySensorVoltageToNinebot(sensorValue);
+
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  DAC.outputVoltage(1000);
-  Serial.println(String(count, 10));
+  DAC.outputVoltage(analogOutput);
+  Serial.print("Count: A: ");
+  Serial.print(String(count++, 10));
+  Serial.print(", Input: ");
+  Serial.print(String(sensorValue, 10));
+  Serial.print(", Output: ");
+  Serial.println(String(analogOutput, 10));
+
   delay(1000);                      // wait for a second
 
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  DAC.outputVoltage(3500);
+  // DAC.outputVoltage(3500);
 
   delay(1000);                      // wait for a second
-  Serial.print("Count: A: ");
-  Serial.println(String(count++, 10));
-  Serial.println(String(sensorValue, HEX));
 }
